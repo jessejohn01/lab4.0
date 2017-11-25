efa4dd03e3d973e71ab909e53dc15e5a501e6683
jessejohn01 <jesse.arstein@yahoo.com>
2017-11-25 00:30:15 -0700

    Fixed Bug

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	 public static void main(String[] args) {
 		 
 		 DivideAndConquer mainInstance = new DivideAndConquer(); // Creates the instance that runs the algorithm.
-		 
+		 mainInstance.buildNodes();
 		 
 	 }
 	

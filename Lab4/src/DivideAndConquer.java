0de52b98aecb3394de24c0f4f032269e98e88fe6
Kyle Webster <kyle1234webster@gmail.com>
2017-11-28 10:28:20 -0700

    Update DivideAndConquer.java

--- Lab4/src/DivideAndConquer.java
+++ Lab4/src/DivideAndConquer.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.Math;
 
 public class DivideAndConquer {
 	String inputFilePath = "input.txt"; // Easy filename access.
@@ -20,7 +21,6 @@ public class DivideAndConquer {
 			System.out.println("File Not Found.");
 		}
 		return lineCount;
-
 	}
 
 	private String[] readInStrings() { // Method to return a string array of the nodes.
@@ -37,8 +37,6 @@ public class DivideAndConquer {
 			System.out.println("File Not Found");
 		}
 		return inputStringArray;
-
-
 	}
 
 	public void buildNodes() { // Method to build the nodes and index them
@@ -51,7 +49,8 @@ public class DivideAndConquer {
 		}
 		System.out.println("Nodes have been indexed.");
 	}
-public Node[] getNodeArray() {
+
+	public Node[] getNodeArray() {
 		return nodeArray;
 	}
 
@@ -61,44 +60,39 @@ public Node[] getNodeArray() {
 		int n2 = r - m;
 		Node L[] = new Node [n1];
 		Node R[] = new Node [n2];
-		for (int i=0; i<n1; i++)
+		for (int i=0; i<n1; i++) {
 			L[i] = arr[l + i];
-		for (int j=0; j<n2; j++)
-			R[j] = arr[m + 1+ j];
+		}
+		for (int j=0; j<n2; j++) {
+			R[j] = arr[m + 1 + j];
+		}
 		int i = 0, j = 0;
 		int k = l;
-		while (i < n1 && j < n2)
-		{
-			if (L[i].getX() <= R[j].getX())
-			{
+		while (i < n1 && j < n2) {
+			if (L[i].getX() <= R[j].getX()) {
 				arr[k] = L[i];
 				i++;
 			}
-			else if(L[i].getX()> R[j].getX())
-			{
+			else if(L[i].getX()> R[j].getX()) {
 				arr[k] = R[j];
 				j++;
 			}
-			else if(L[i].getY()> R[j].getY())
-			{
+			else if(L[i].getY()> R[j].getY()) {
 				arr[k] = R[j];
 				j++;
 			}
-			else
-			{
+			else {
 				arr[k] = L[i];
 				i++;
 			}
 			k++;
 		}
-		while (i < n1)
-		{
+		while (i < n1) {
 			arr[k] = L[i];
 			i++;
 			k++;
 		}
-		while (j < n2)
-		{
+		while (j < n2) {
 			arr[k] = R[j];
 			j++;
 			k++;
@@ -117,8 +111,40 @@ public Node[] getNodeArray() {
 		}
 		return inNode;
 	}
+
 	public void findSmallestDistance(Node[] inNode, int l, int r)
 	{
+		int m = (l + r) / 2;
+		if(l < r-1) {
+			System.out.println("Solving Problem at Point[" + l + "]... Point[" + r + "]");
+			System.out.println("Dividing at Point[" + m + "]");
+			findSmallestDistance(inNode, l, m);
+			System.out.println("Solving Problem at Point[" + l + "]... Point[" + m + "]");
+			double left = distanceFormula(inNode, l, m);
+			findSmallestDistance(inNode, m + 1, r);
+			System.out.println("Solving Problem at Point[" +(m+1) + "]... Point[" + r + "]");
+			double right = distanceFormula(inNode, m + 1, r);
+		}
+	}
 
+	public double distanceFormula(Node[] inNode, int l, int r)
+	{
+		double length = 0;
+		if(l == r)
+		{
+			length = 999999999;
+			return length;
+		}
+		else{
+			double x1 = inNode[l].getX();
+			double x2 = inNode[r].getX();
+			double y1 = inNode[l].getY();
+			double y2 = inNode[r].getY();
+			double xLength = Math.pow(x2 - x1, 2);
+			double yLength = Math.pow(y2 - y1, 2);
+			length = Math.pow(xLength+yLength, .5);
+			System.out.println(length);
+			return length;
+		}
 	}
 }

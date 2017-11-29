ad61627b4ad53ea0bf568c7d2d1afb89ef4909b5
Kyle Webster <kyle1234webster@gmail.com>
2017-11-29 08:50:06 -0700

    Update Main.java

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -1,9 +1,11 @@
+/**
+ * Kyle Webster and Jesse Arstein
+ * Finished November 28, 2017
+ */
 public class Main {
 	 public static void main(String[] args) {
 		 DivideAndConquer mainInstance = new DivideAndConquer(); // Creates the instance that runs the algorithm.
-		 mainInstance.buildNodes();
-		 Node[] array = mainInstance.getNodeArray();
-		 array = mainInstance.sort(array, 0, array.length-1);
-		 mainInstance.findSmallestDistance(array, 0, array.length-1);
+		 mainInstance.buildNodes(); //builds the Nodes from the list of points
+		 mainInstance.shortestDistance(); //finds the points that have the shortest distance from each other
 	 }
 }
